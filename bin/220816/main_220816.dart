c740002a9bec466374d22050e53dc409f08efe4d
Marc Renken <marc.renken@bitbot.eu>
2022-08-16 19:10:12 +0200

    Day 220816

--- bin/220816/main_220816.dart
+++ bin/220816/main_220816.dart
@@ -1,5 +1,28 @@
 void main() {
   print('Day 0 - 220816');
+
+  final k = 17;
+
+  print(twoAddUpToKbrutForce([10, 15, 3, 7], k));
+  print(twoAddUpToKbrutForce([10, 15, 3, 8], k));
 }
 
 String doSomething() => '220816';
+
+bool twoAddUpToKbrutForce(List<int> list, int k) {
+  for (final i in list.asMap().keys) {
+    for (final j in list.asMap().keys) {
+      if (i != j && list[i] + list[j] == k) return true;
+    }
+  }
+  return false;
+}
+
+bool twoAddUpToK(List<int> list, int k) {
+  final map = <int, int>{};
+  for (final i in list) {
+    if (map.containsKey(k - i)) return true;
+    map[i] = i;
+  }
+  return false;
+}
--- test/220817/day_220817_test.dart
+++ test/220817/day_220817_test.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 import '../../bin/220817/main_220817.dart';
 
 void main() {
-  group('Day -1 - 220817', () {
+  group('Day 1 - 220817', () {
     test('Day 220817 - doSomething', () {
       expect(doSomething(), '220817');
     });
