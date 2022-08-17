0189f852a30880b43efccef2685a33e6fb365e1e
Marc Renken <marc.renken@bitbot.eu>
2022-08-17 21:49:50 +0200

    Day 220817

--- bin/220817/main_220817.dart
+++ bin/220817/main_220817.dart
@@ -1,5 +1,31 @@
 void main() {
   print('Day 1 - 220817');
+
+  print(productsOfEveryOtherNumber([1, 2, 3, 4, 5]));
+  print(productsOfEveryOtherNumberHard([1, 2, 3, 4, 5]));
 }
 
 String doSomething() => '220817';
+
+List<int> productsOfEveryOtherNumber(List<int> list) {
+  final res = <int>[];
+  final product = list.reduce((value, element) => value * element);
+  for (final e in list) {
+    res.add(product ~/ e);
+  }
+  return res;
+}
+
+
+/// Implements productsOfEveryOtherNumber without division
+List<int> productsOfEveryOtherNumberHard(List<int> list) {
+  final res = <int>[];
+  for (var i = 0; i < list.length; i++) {
+    var product = 1;
+    for (var j = 0; j < list.length; j++) {
+      if (j != i) product *= list[j];
+    }
+    res.add(product);
+  }
+  return res;
+}
