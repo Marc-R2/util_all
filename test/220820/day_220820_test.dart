31bfeb02f28d94fccf307ac2faaf4dcbc0fbf291
Marc Renken <marc.renken@bitbot.eu>
2022-08-20 20:36:04 +0200

    Day 220820 - Tests

--- test/220820/day_220820_test.dart
+++ test/220820/day_220820_test.dart
@@ -6,5 +6,13 @@ void main() {
     test('Day 220820 - doSomething', () {
       expect(doSomething(), '220820');
     });
+
+    test('Day 4 - 22082 - car', () {
+      expect(car(cons(3, 4)), 3);
+    });
+
+    test('Day 4 - 22082 - cdr', () {
+      expect(cdr(cons(3, 4)), 4);
+    });
   });
 }
