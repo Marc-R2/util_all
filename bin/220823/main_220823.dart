16b67f383553eaf237e788daa0c857621e90fc5b
Marc Renken <marc.renken@bitbot.eu>
2022-08-23 17:58:39 +0200

    Day 220823 - UniValTree

--- bin/220823/main_220823.dart
+++ bin/220823/main_220823.dart
@@ -1,5 +1,8 @@
 void main() {
   print('Day 7 - 220823');
+
+  print(sampleTree.isUniVal);
+  print(sampleTree.countUniVal());
 }
 
 String doSomething() => '220823';
@@ -16,6 +19,17 @@ class Node<T> {
   final Node<T>? left;
 
   final Node<T>? right;
+
+  bool get isUniVal =>
+      (left == null || left!.value == value) &&
+      (right == null || right!.value == value);
+
+  int countUniVal() {
+    var counter = isUniVal ? 1 : 0;
+    if (left != null) counter += left!.countUniVal();
+    if (right != null) counter += right!.countUniVal();
+    return counter;
+  }
 }
 
 const sampleTree = Node<int>(
