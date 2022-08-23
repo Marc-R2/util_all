d7c709c584c88683accb7e5acccda4ebe14f0f21
Marc Renken <marc.renken@bitbot.eu>
2022-08-23 17:52:46 +0200

    Day 220823 - Init

--- bin/220823/main_220823.dart
+++ bin/220823/main_220823.dart
@@ -3,3 +3,31 @@ void main() {
 }
 
 String doSomething() => '220823';
+
+class Node<T> {
+  const Node({
+    required this.value,
+    this.left,
+    this.right,
+  });
+
+  final T value;
+
+  final Node<T>? left;
+
+  final Node<T>? right;
+}
+
+const sampleTree = Node<int>(
+  value: 0,
+  left: Node(value: 1),
+  right: Node(
+    value: 0,
+    left: Node(
+      value: 1,
+      left: Node(value: 1),
+      right: Node(value: 1),
+    ),
+    right: Node(value: 0),
+  ),
+);
