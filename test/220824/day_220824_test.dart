cb3600505076e8e678c7cbfb94de00705a485c04
Marc Renken <marc.renken@bitbot.eu>
2022-08-25 15:05:30 +0200

    Day 220824 - Unittests

--- test/220824/day_220824_test.dart
+++ test/220824/day_220824_test.dart
@@ -6,5 +6,51 @@ void main() {
     test('Day 220824 - doSomething', () {
       expect(doSomething(), '220824');
     });
+
+    group('Day 220824 - sumLargestNonAdjacentNums', () {
+      test('Day 220824 - [2, 4, 6, 2, 5] (sample 1)', () {
+        expect(sumLargestNonAdjacentNums([2, 4, 6, 2, 5]), 13);
+      });
+
+      test('Day 220824 - [5, 1, 1, 5] (sample 2)', () {
+        expect(sumLargestNonAdjacentNums([5, 1, 1, 5]), 10);
+      });
+
+      test('Day 220824 - []', () {
+        expect(sumLargestNonAdjacentNums([]), 0);
+      });
+
+      test('Day 220824 - [1]', () {
+        expect(sumLargestNonAdjacentNums([1]), 1);
+      });
+
+      test('Day 220824 - [2]', () {
+        expect(sumLargestNonAdjacentNums([2]), 2);
+      });
+
+      test('Day 220824 - [1, 2]', () {
+        expect(sumLargestNonAdjacentNums([1, 2]), 2);
+      });
+
+      test('Day 220824 - [1, 2, 3]', () {
+        expect(sumLargestNonAdjacentNums([1, 2, 3]), 4);
+      });
+
+      test('Day 220824 - [1, 2, 3, 4]', () {
+        expect(sumLargestNonAdjacentNums([1, 2, 3, 4]), 6);
+      });
+
+      test('Day 220824 - [1, 2, 3, 4, 5]', () {
+        expect(sumLargestNonAdjacentNums([1, 2, 3, 4, 5]), 9);
+      });
+
+      test('Day 220824 - [1, 2, 3, 4, 5, 6]', () {
+        expect(sumLargestNonAdjacentNums([1, 2, 3, 4, 5, 6]), 12);
+      });
+
+      test('Day 220824 - [1, 2, 3, 4, 5, 6, 7]', () {
+        expect(sumLargestNonAdjacentNums([1, 2, 3, 4, 5, 6, 7]), 16);
+      });
+    });
   });
 }
