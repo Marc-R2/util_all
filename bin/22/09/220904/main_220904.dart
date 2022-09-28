9b829e30ce88f928fe3da9f36eb733e6c25308a2
Marc Renken <marc.renken@bitbot.eu>
2022-09-28 16:04:38 +0200

    Day 220904 - Fix && Tests

--- bin/22/09/220904/main_220904.dart
+++ bin/22/09/220904/main_220904.dart
@@ -7,7 +7,7 @@ void main() {
 
   print(list1);
   print(list2);
-  
+
   print(ListNode.findFirstIntersection(list1, list2));
 }
 
@@ -22,14 +22,14 @@ class ListNode {
   @override
   String toString() => next == null ? '$data' : '$data, $next';
 
-  static int? findFirstIntersection(ListNode list1, ListNode list2) {
-    final l1 = list1.next ?? list1;
-    final l2 = list2.next ?? list2;
-    if (!(list1.next == null && list2.next == null)) {
-      final res = findFirstIntersection(l1, l2);
+  static int? findFirstIntersection(ListNode l1, ListNode l2) {
+    if (!(l1.next == null && l2.next == null)) {
+      final res = findFirstIntersection(l1.next ?? l1, l2.next ?? l2);
       if (res != null) return res;
     }
     if (l1.data == l2.data) return null;
-    return l1.next != null ? l1.next!.data : null;
+    return l1.next != null && l2.next != null && l1.next!.data == l2.next!.data
+        ? l1.next!.data
+        : -1;
   }
 }
--- test/22/09/220904/day_220904_test.dart
+++ test/22/09/220904/day_220904_test.dart
@@ -6,5 +6,80 @@ void main() {
     test('Day 220904 - doSomething', () {
       expect(doSomething(), '220904');
     });
+
+    test('Day 220904 - ListNode 001', () {
+      const pair = ListNode(8, ListNode(10));
+      const list1 = ListNode(3, ListNode(7, pair));
+      const list2 = ListNode(99, ListNode(1, pair));
+      expect(list1.toString(), '3, 7, 8, 10');
+      expect(list2.toString(), '99, 1, 8, 10');
+      expect(ListNode.findFirstIntersection(list1, list2), 8);
+    });
+
+    test('Day 220904 - ListNode 002', () {
+      const pair = ListNode(8, ListNode(10));
+      const list1 = ListNode(3, ListNode(7, pair));
+      const list2 = ListNode(99, ListNode(1, ListNode(8, ListNode(10))));
+      expect(list1.toString(), '3, 7, 8, 10');
+      expect(list2.toString(), '99, 1, 8, 10');
+      expect(ListNode.findFirstIntersection(list1, list2), 8);
+    });
+
+    test('Day 220904 - ListNode 003', () {
+      const pair = ListNode(8, ListNode(10));
+      const list1 = ListNode(3, ListNode(7, ListNode(8, ListNode(10))));
+      const list2 = ListNode(99, ListNode(1, pair));
+      expect(list1.toString(), '3, 7, 8, 10');
+      expect(list2.toString(), '99, 1, 8, 10');
+      expect(ListNode.findFirstIntersection(list1, list2), 8);
+    });
+
+    test('Day 220904 - ListNode 004', () {
+      const list1 = ListNode(3, ListNode(7, ListNode(8, ListNode(10))));
+      const list2 = ListNode(99, ListNode(1, ListNode(8, ListNode(10))));
+      expect(list1.toString(), '3, 7, 8, 10');
+      expect(list2.toString(), '99, 1, 8, 10');
+      expect(ListNode.findFirstIntersection(list1, list2), 8);
+    });
+
+    test('Day 220904 - ListNode 005', () {
+      const list1 = ListNode(3, ListNode(7, ListNode(9, ListNode(10))));
+      const list2 = ListNode(99, ListNode(1, ListNode(8, ListNode(10))));
+      expect(list1.toString(), '3, 7, 9, 10');
+      expect(list2.toString(), '99, 1, 8, 10');
+      expect(ListNode.findFirstIntersection(list1, list2), 10);
+    });
+
+    test('Day 220904 - ListNode 006', () {
+      const list1 = ListNode(3, ListNode(7, ListNode(9, ListNode(10))));
+      const list2 = ListNode(99, ListNode(1, ListNode(8, ListNode(11))));
+      expect(list1.toString(), '3, 7, 9, 10');
+      expect(list2.toString(), '99, 1, 8, 11');
+      expect(ListNode.findFirstIntersection(list1, list2), -1);
+    });
+
+    test('Day 220904 - ListNode 007', () {
+      const list1 = ListNode(3, ListNode(7, ListNode(9, ListNode(10))));
+      const list2 = ListNode(99, ListNode(7, ListNode(8, ListNode(11))));
+      expect(list1.toString(), '3, 7, 9, 10');
+      expect(list2.toString(), '99, 7, 8, 11');
+      expect(ListNode.findFirstIntersection(list1, list2), -1);
+    });
+
+    test('Day 220904 - ListNode 008', () {
+      const list1 = ListNode(3, ListNode(7, ListNode(9, ListNode(10))));
+      const list2 = ListNode(99, ListNode(7, ListNode(9, ListNode(11))));
+      expect(list1.toString(), '3, 7, 9, 10');
+      expect(list2.toString(), '99, 7, 9, 11');
+      expect(ListNode.findFirstIntersection(list1, list2), -1);
+    });
+
+    test('Day 220904 - ListNode 009', () {
+      const list1 = ListNode(3, ListNode(7, ListNode(9, ListNode(10))));
+      const list2 = ListNode(99, ListNode(7, ListNode(9, ListNode(10))));
+      expect(list1.toString(), '3, 7, 9, 10');
+      expect(list2.toString(), '99, 7, 9, 10');
+      expect(ListNode.findFirstIntersection(list1, list2), 7);
+    });
   });
 }
